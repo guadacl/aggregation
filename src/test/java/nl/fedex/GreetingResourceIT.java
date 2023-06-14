01020ef321959e87cb7fa382778037b3fe9de783
quarkusio <no-reply@quarkus.io>
2023-06-14 07:14:38 +0000

    Initial commit

---
+++ src/test/java/nl/fedex/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package nl.fedex;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
